3083eac024aab26d90ad1085d3abfe4cba37b424
lucas_cimino <lucasciminoalves@gmail.com>
2026-05-27 11:35:59 -0300

    feat: adiciona alerta de confirmacao para exclusao de missoes com cores customizadas

--- app/src/main/java/com/example/routinequestmobile/TaskAdapter.java
+++ app/src/main/java/com/example/routinequestmobile/TaskAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 import java.util.List;
 
@@ -45,7 +47,37 @@ public class TaskAdapter extends ArrayAdapter<Task> {
             btnDeleteTask.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    deleteListener.onDeleteClick(currentTask);
+
+                    // 1. Cria a caixa de diálogo, mas guarda ela numa variável em vez de mostrar na hora
+                    android.app.AlertDialog dialog = new android.app.AlertDialog.Builder(v.getContext())
+                            .setTitle("Abandonar Missão?")
+                            .setMessage("Tem certeza que deseja excluir esta tarefa? Você não receberá XP por ela.")
+
+                            .setPositiveButton("Sim, Excluir", new android.content.DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(android.content.DialogInterface dialogInterface, int which) {
+                                    deleteListener.onDeleteClick(currentTask);
+                                }
+                            })
+
+                            .setNegativeButton("Cancelar", new android.content.DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(android.content.DialogInterface dialogInterface, int which) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .create(); // Cria o alerta em vez de mostrar direto
+
+                    // 2. Exibe o alerta na tela primeiro
+                    dialog.show();
+
+                    // Botão Positivo (Sim, Excluir) -> Vermelho
+                    dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE)
+                            .setTextColor(android.graphics.Color.parseColor("#E53935"));
+
+                    // Botão Negativo (Cancelar) -> Azul
+                    dialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE)
+                            .setTextColor(android.graphics.Color.parseColor("#5A7FD4"));
                 }
             });
         }
